eed1a9fb948f1e037327a6355cc67e51561d8b0f
Caleb Bennetts <82233759+c675b713@users.noreply.github.com>
2021-10-22 14:18:12 -0500

    changed png to svg files
    also fixed alternating layout of black and white grid

--- gameConfig/pieceImg.gameConfig.js
+++ gameConfig/pieceImg.gameConfig.js
@@ -1,19 +1,19 @@
 export const pieceImg = {
-    'king_black': './img/king_black.png',
-    'king_white': './img/king_white.png', 
+    'king_black': './img/king_black.svg',
+    'king_white': './img/king_white.svg', 
 
-    'queen_black': './img/queen_black.png',
-    'queen_white': './img/queen_white.png', 
+    'queen_black': './img/queen_black.svg',
+    'queen_white': './img/queen_white.svg', 
 
-    'knight_black': './img/knight_black.png',
-    'knight_white': './img/knight_white.png',
+    'knight_black': './img/knight_black.svg',
+    'knight_white': './img/knight_white.svg',
 
-    'rook_black': './img/rook_black.png',
-    'rook_white': './img/rook_white.png',
+    'rook_black': './img/rook_black.svg',
+    'rook_white': './img/rook_white.svg',
 
-    'bishop_black': './img/bishop_black.png',
-    'bishop_white': './img/bishop_white.png',
+    'bishop_black': './img/bishop_black.svg',
+    'bishop_white': './img/bishop_white.svg',
 
-    'pawn_black': './img/pawn_black.png',
-    'pawn_white': './img/pawn_white.png',
+    'pawn_black': './img/pawn_black.svg',
+    'pawn_white': './img/pawn_white.svg',
 }
