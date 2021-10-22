097f0773457bfdf184f762c05bdd56f1b71e0cea
Caleb Bennetts <82233759+c675b713@users.noreply.github.com>
2021-10-22 14:00:54 -0500

    added img, grid layout
    -added piece images
    -added a div element 8x8 grid that is working just doesn't look right
    -set up someone of a module system so we can easily change parts and merge together

---
+++ gameConfig/pieceImg.gameConfig.js
@@ -0,0 +1,19 @@
+export const pieceImg = {
+    'king_black': './img/king_black.png',
+    'king_white': './img/king_white.png', 
+
+    'queen_black': './img/queen_black.png',
+    'queen_white': './img/queen_white.png', 
+
+    'knight_black': './img/knight_black.png',
+    'knight_white': './img/knight_white.png',
+
+    'rook_black': './img/rook_black.png',
+    'rook_white': './img/rook_white.png',
+
+    'bishop_black': './img/bishop_black.png',
+    'bishop_white': './img/bishop_white.png',
+
+    'pawn_black': './img/pawn_black.png',
+    'pawn_white': './img/pawn_white.png',
+}
